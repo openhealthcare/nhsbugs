c21d81731ad9b60335f88e90977fedd1e10cc95c
Ross Jones <rossdjones@gmail.com>
2012-05-26 11:04:42 +0100

    Updated requirements and initial model for hospital

--- nhsbugs/facilities/models.py
+++ nhsbugs/facilities/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class SHA(models.Model):
+
+    code = models.CharField(max_length=10, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    code = models.CharField(max_length=100, null=False, blank=False)
+
+    sha_code = models.CharField(max_length=32, null=True, blank=True)
+    address1 = models.CharField(max_length=64, null=False, blank=False)
+    address2 = models.CharField(max_length=64, null=False, blank=False)
+    address3 = models.CharField(max_length=64, null=True, blank=True)
+    address4 = models.CharField(max_length=64, null=True, blank=True)
+    address5 = models.CharField(max_length=64, null=True, blank=True)
+    postcode = models.CharField(max_length=16, null=True, blank=True)
+
+    open_date = models.DateTimeField(null=True, blank=True)
+    close_date = models.DateTimeField(null=True, blank=True)
+
+
+
+class Hospital(models.Model):
+
+    code = models.CharField(max_length=10, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    sha_code = models.CharField(max_length=32, null=True, blank=True)
+    address1 = models.CharField(max_length=64, null=False, blank=False)
+    address2 = models.CharField(max_length=64, null=False, blank=False)
+    address3 = models.CharField(max_length=64, null=True, blank=True)
+    address4 = models.CharField(max_length=64, null=True, blank=True)
+    address5 = models.CharField(max_length=64, null=True, blank=True)
+    postcode = models.CharField(max_length=16, null=True, blank=True)
+
+    open_date = models.DateTimeField(null=True, blank=True)
+    close_date = models.DateTimeField(null=True, blank=True)
+
--- nhsbugs/nhsbugs/settings.py
+++ nhsbugs/nhsbugs/settings.py
@@ -48,27 +48,13 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+
+# MEDIA
 MEDIA_ROOT = os.path.join( PROJECT_ROOT, "../static" )
-print MEDIA_ROOT
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = 'media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
@@ -80,7 +66,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -106,9 +91,6 @@ ROOT_URLCONF = 'nhsbugs.urls'
 WSGI_APPLICATION = 'nhsbugs.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     os.path.join( PROJECT_ROOT, "../templates" )
 )
 
@@ -126,6 +108,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'facilities'
 )
 
 # A sample logging configuration. The only tangible logging
