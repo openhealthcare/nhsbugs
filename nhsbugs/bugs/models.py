a0f5dcd17e191b8883620ab932d733cc282080d5
Ross Jones <rossdjones@gmail.com>
2012-05-26 12:27:54 +0100

    Updated close dates

--- nhsbugs/bugs/models.py
+++ nhsbugs/bugs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bug(models.Model):
+    summary = models.CharField(max_length=120, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    reporter = models.ForeignKeyField(User)
+    report_date = models.DateTimeField(auto_now=False, auto_now_add=True)
--- nhsbugs/facilities/views.py
+++ nhsbugs/facilities/views.py
@@ -5,23 +5,36 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404, HttpRespons
 from django.shortcuts import render_to_response, redirect, get_object_or_404, render
 from django.template import RequestContext
 
+from models import SHA, Hospital
 
 def list_hospitals(request):
+    hospitals = Hospital.objects.all()
     return render_to_response('facilities/hospital_list.html',
-                               {},
+                               {
+                                 'hospitals': hospital
+                               },
                                context_instance=RequestContext(request))
 
 def view_hospital(request, slug):
+    hospital = get_object_or_404( Hospital, slug=slug)
     return render_to_response('facilities/hospital_view.html',
-                               {},
+                               {
+                                  "hospital": hospital
+                               },
                                context_instance=RequestContext(request))
 
 def list_shas(request):
+    shas = SHA.objects.all()
     return render_to_response('facilities/sha_list.html',
-                               {},
+                               {
+                                'shas': shas
+                               },
                                context_instance=RequestContext(request))
 
 def view_sha(request, slug):
+    sha = get_object_or_404( SHA, slug=slug)
     return render_to_response('facilities/sha_view.html',
-                               {},
+                               {
+                                "sha": sha
+                               },
                                context_instance=RequestContext(request))
