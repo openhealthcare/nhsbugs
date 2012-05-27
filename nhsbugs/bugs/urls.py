9efcf59e406911008677ecd8a4e8c1b4af8aeaa0
Ross Jones <rossdjones@gmail.com>
2012-05-27 14:07:04 +0100

    Fixing my dodgy naming of template vars

--- nhsbugs/bugs/urls.py
+++ nhsbugs/bugs/urls.py
@@ -6,7 +6,7 @@ from views import view_bug, report_bug, vote_bug, view_my_bugs
 
 urlpatterns = patterns('',
     url(r'^report/$', report_bug, name='report_bug'),
-    url(r'^report/(?P<hospital_slug>[\w-]+)$', report_bug, name='report_bug'),
+    url(r'^report/(?P<hospital_slug>[\w-]+)$', report_bug, name='report_bug_hospital'),
     url(r'^view/(?P<slug>[\w-]+)$', view_bug, name='view_bug'),
     url(r'^vote/(?P<slug>[\w-]+)/(?P<score>.*)/$', vote_bug, name='vote_bug'),
     url(r'^my-bugs/$', view_my_bugs, name='my_bugs'),
